7021a0ec62eb8af0b14d667604ed844c5a88342f
Liutauras <lidi.uk@gmail.com>
2015-10-16 02:40:02 +0200

    PO: Enable buttons. Add toast for the capstone button

--- app/src/main/java/pm/diu/liutauras/myportfolioapp/MainActivity.java
+++ app/src/main/java/pm/diu/liutauras/myportfolioapp/MainActivity.java
@@ -1,5 +1,6 @@
 package pm.diu.liutauras.myportfolioapp;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -10,6 +11,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,5 +43,34 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void launchSpotifyStreamer(View view) {
+        //Placeholder for SpotifyStreamerLauncher
+    }
+
+    public void launchScoresApp(View view) {
+        //Placeholder for ScoresAppLauncher
+    }
+
+    public void launchLibraryApp(View view) {
+        //Placeholder for LibraryAppLauncher
+    }
+
+    public void launchBuilditBigger(View view) {
+        //Placeholder for BuilditBiggerLauncher
+    }
+
+    public void launchXyzReader(View view) {
+        //Placeholder for XyzReaderLauncher
+    }
+
+    public void launchCapstoneApp(View view) {
+        //Placeholder for CapstoneAppLauncher
+        //Toast
+        Context context = getApplicationContext();
+        CharSequence text = getString(R.string.capstoneToastMsg);
+        int duration = Toast.LENGTH_SHORT;
+        Toast.makeText(context, text, duration).show();
     }
 }
